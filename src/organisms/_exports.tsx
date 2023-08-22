348c23c999dd92056cce4d59a3a6d3b9b562efa2
mcgear <michael.gearhardt@gmail.com>
2023-08-21 23:17:34 -0600

    Initial pass through components

---
+++ src/organisms/_exports.tsx
@@ -0,0 +1,19 @@
+export {
+  Features,
+  type FeaturesProps,
+  Footer,
+  type FooterProps,
+  Header,
+  HeaderLogo,
+  type HeaderProps,
+  Hero,
+  type HeroProps,
+  HeroStyleTypes,
+  type StepFeature,
+  StepsFeatures,
+  type StepsFeaturesProps,
+} from "../src.deps.ts";
+
+export { FeedCard, type FeedCardProps } from "../src.deps.ts";
+
+export { PostForm, type PostFormProps } from "../src.deps.ts";
