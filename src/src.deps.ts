50e1db77ae601182a7221bdc7676cf4201a00b2d
mcgear <michael.gearhardt@gmail.com>
2024-01-08 12:49:09 -0700

    upgrades

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -2,6 +2,6 @@
 // export * from "../../../fathym-deno/atomic-chat/mod.ts";
 // export * from "../../../fathym-deno/atomic-social/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic@v0.0.93-integration/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_chat@v0.0.21-integration/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_social@v0.0.45-integration/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.95/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_chat@v0.0.22/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_social@v0.0.46/mod.ts";
