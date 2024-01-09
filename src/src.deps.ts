48ec480f0711dad24dcbdb07301022d1ea8c4109
mcgear <michael.gearhardt@gmail.com>
2024-01-09 11:01:16 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -4,11 +4,11 @@
 
 export * from "https://deno.land/x/fathym_common@v0.0.127/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic@v0.0.100/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_chat@v0.0.23/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_iot@v0.0.40/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_reports@v0.0.13/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_social@v0.0.47/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.101/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_chat@v0.0.24/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_iot@v0.0.41/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_reports@v0.0.14/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_social@v0.0.48/mod.ts";
 
 import FATC from "https://deno.land/x/fathym_atomic@v0.0.100/tailwind.components.ts";
 export const FathymAtomicTailwindComponents = FATC;
