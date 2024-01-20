aa298a7f68b9823667098a7457fdec79585e12bc
mcgear <michael.gearhardt@gmail.com>
2024-01-20 02:21:24 -0700

    ref updates

--- tailwind.components.ts
+++ tailwind.components.ts
@@ -13,7 +13,7 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic@v0.0.111/',
+		Root: 'https://deno.land/x/fathym_atomic@v0.0.112/',
 		Components: [
 			"./src/atoms/Action.tsx",
 			"./src/atoms/forms/Input.tsx",
