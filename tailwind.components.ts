2a88da5ce40e62ae71a9b43756ec361aeb493967
mcgear <michael.gearhardt@gmail.com>
2024-01-25 11:44:36 -0700

    ref updates

--- tailwind.components.ts
+++ tailwind.components.ts
@@ -19,7 +19,7 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic@v0.0.112/',
+		Root: 'https://deno.land/x/fathym_atomic@v0.0.114/',
 		Components: [
 			"./src/atoms/Action.tsx",
 			"./src/atoms/forms/Input.tsx",
@@ -41,7 +41,7 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_chat@v0.0.25/',
+		Root: 'https://deno.land/x/fathym_atomic_chat@v0.0.26/',
 		Components: [
 			"./src/molecules/ChatBox.tsx",
 			"./src/molecules/ChatInput.tsx",
@@ -49,13 +49,13 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_iot@v0.0.42/',
+		Root: 'https://deno.land/x/fathym_atomic_iot@v0.0.43/',
 		Components: [
 			"./src/molecules/IoTDisplay.tsx",
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_reports@v0.0.15/',
+		Root: 'https://deno.land/x/fathym_atomic_reports@v0.0.16/',
 		Components: [
 			"./src/molecules/BasicSummary.tsx",
 			"./src/molecules/BulletList.tsx",
@@ -71,7 +71,7 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_social@v0.0.49/',
+		Root: 'https://deno.land/x/fathym_atomic_social@v0.0.50/',
 		Components: [
 			"./src/organisms/FeedCard.tsx",
 			"./src/organisms/FeedCardList.tsx",
