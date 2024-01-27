6f1974397853d4ae9f8dd701e23c20e602884cf2
mcgear <michael.gearhardt@gmail.com>
2024-01-26 20:51:52 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -2,25 +2,25 @@
 // export * from "../../../fathym-deno/atomic-chat/mod.ts";
 // export * from "../../../fathym-deno/atomic-social/mod.ts";
 
-export * from "https://deno.land/x/fathym_common@v0.0.134/mod.ts";
+export * from "https://deno.land/x/fathym_common@v0.0.137/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic@v0.0.115/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_chat@v0.0.27/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_iot@v0.0.44/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_reports@v0.0.17/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_social@v0.0.51/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.117/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_chat@v0.0.28/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_iot@v0.0.45/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_reports@v0.0.18/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_social@v0.0.52/mod.ts";
 
-import FATC from "https://deno.land/x/fathym_atomic@v0.0.115/tailwind.components.ts";
+import FATC from "https://deno.land/x/fathym_atomic@v0.0.117/tailwind.components.ts";
 export const FathymAtomicTailwindComponents = FATC;
 
-import FACTC from "https://deno.land/x/fathym_atomic_chat@v0.0.27/tailwind.components.ts";
+import FACTC from "https://deno.land/x/fathym_atomic_chat@v0.0.28/tailwind.components.ts";
 export const FathymAtomicChatTailwindComponents = FACTC;
 
-import FAITC from "https://deno.land/x/fathym_atomic_iot@v0.0.44/tailwind.components.ts";
+import FAITC from "https://deno.land/x/fathym_atomic_iot@v0.0.45/tailwind.components.ts";
 export const FathymAtomicIoTTailwindComponents = FAITC;
 
-import FARTC from "https://deno.land/x/fathym_atomic_reports@v0.0.17/tailwind.components.ts";
+import FARTC from "https://deno.land/x/fathym_atomic_reports@v0.0.18/tailwind.components.ts";
 export const FathymAtomicReportsTailwindComponents = FARTC;
 
-import FASTC from "https://deno.land/x/fathym_atomic_social@v0.0.51/tailwind.components.ts";
+import FASTC from "https://deno.land/x/fathym_atomic_social@v0.0.52/tailwind.components.ts";
 export const FathymAtomicSocialTailwindComponents = FASTC;
--- tailwind.components.ts
+++ tailwind.components.ts
@@ -19,7 +19,7 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic@v0.0.115/',
+		Root: 'https://deno.land/x/fathym_atomic@v0.0.117/',
 		Components: [
 			"./src/atoms/Action.tsx",
 			"./src/atoms/forms/Input.tsx",
@@ -41,7 +41,7 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_chat@v0.0.27/',
+		Root: 'https://deno.land/x/fathym_atomic_chat@v0.0.28/',
 		Components: [
 			"./src/molecules/ChatBox.tsx",
 			"./src/molecules/ChatInput.tsx",
@@ -49,13 +49,13 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_iot@v0.0.44/',
+		Root: 'https://deno.land/x/fathym_atomic_iot@v0.0.45/',
 		Components: [
 			"./src/molecules/IoTDisplay.tsx",
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_reports@v0.0.17/',
+		Root: 'https://deno.land/x/fathym_atomic_reports@v0.0.18/',
 		Components: [
 			"./src/molecules/BasicSummary.tsx",
 			"./src/molecules/BulletList.tsx",
@@ -71,7 +71,7 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_social@v0.0.51/',
+		Root: 'https://deno.land/x/fathym_atomic_social@v0.0.52/',
 		Components: [
 			"./src/organisms/FeedCard.tsx",
 			"./src/organisms/FeedCardList.tsx",
