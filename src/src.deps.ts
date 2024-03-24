f89480ee16874eb2eaee1ce3cc07347bbfaafaed
mcgear <michael.gearhardt@gmail.com>
2024-03-24 17:21:01 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -9,7 +9,7 @@ export * from "https://esm.sh/preact@10.20.1/hooks";
 
 export * from "https://deno.land/x/fathym_common@v0.0.177/mod.ts";
 // export { Icon } from "../../atomic-icons/browser.ts";
-export { Icon } from "https://deno.land/x/fathym_atomic_icons@v0.0.30/browser.ts";
+export { Icon } from "https://deno.land/x/fathym_atomic_icons@v0.0.31/browser.ts";
 
 export * from "https://deno.land/x/fathym_atomic@v0.0.137/mod.ts";
 export * from "https://deno.land/x/fathym_atomic_chat@v0.0.35/mod.ts";
