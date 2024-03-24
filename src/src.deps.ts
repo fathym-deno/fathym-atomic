1d755cfd05318fb251b4ffa4469e50b5dd95754f
mcgear <michael.gearhardt@gmail.com>
2024-03-24 17:27:24 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -11,10 +11,10 @@ export * from "https://deno.land/x/fathym_common@v0.0.177/mod.ts";
 // export { Icon } from "../../atomic-icons/browser.ts";
 export { Icon } from "https://deno.land/x/fathym_atomic_icons@v0.0.31/browser.ts";
 
-export * from "https://deno.land/x/fathym_atomic@v0.0.137/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_chat@v0.0.35/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_iot@v0.0.52/mod.ts";
-// export * from "https://deno.land/x/fathym_atomic_reports@v0.0.24/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_social@v0.0.57/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.139/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_chat@v0.0.37/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_iot@v0.0.55/mod.ts";
+// export * from "https://deno.land/x/fathym_atomic_reports@v0.0.25/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_social@v0.0.58/mod.ts";
 
 export const IS_BROWSER = typeof document !== "undefined";
