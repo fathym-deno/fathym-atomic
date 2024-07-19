62f22a2c652a7b97e7eea54cbbe001a090ce35ab
mcgear <michael.gearhardt@gmail.com>
2024-07-19 12:15:41 -0600

    missing exports

--- src/molecules/azure/_exports.ts
+++ src/molecules/azure/_exports.ts
@@ -2,6 +2,8 @@ export * from "./CloudConnectExistingForm.tsx";
 
 import CCAF from "./CloudConnectAzureForm.tsx";
 export const CloudConnectAzureForm = CCAF;
+export { type CloudConnectAzureFormProps } from "./CloudConnectAzureForm.tsx";
 
 import CCNSF from "./CloudConnectNewSubForm.tsx";
 export const CloudConnectNewSubForm = CCNSF;
+export { type CloudConnectNewSubFormProps } from "./CloudConnectNewSubForm.tsx";
