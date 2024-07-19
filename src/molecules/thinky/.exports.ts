36db8e9d6d8f9b2a92050727b2b0562ba8babc73
mcgear <michael.gearhardt@gmail.com>
2024-07-19 16:34:04 -0600

    Reorder comment

--- src/molecules/thinky/.exports.ts
+++ src/molecules/thinky/.exports.ts
@@ -1,10 +1,10 @@
 export * from './ChatSet.ts';
 export * from './ChatSets.ts';
 
-// export { ChatInput, type ChatInputProps } from '../../src.deps.ts';
 import ChI from './ChatInput.tsx';
 export const ChatInput = ChI;
 export { type ChatInputProps } from './ChatInput.tsx';
+// export { ChatInput, type ChatInputProps } from '../../src.deps.ts';
 
 import CM from './ChatMessages.tsx';
 export const ChatMessages = CM;
