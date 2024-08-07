4cae277166e6170cecce4eb685333595a35b380f
mcgear <michael.gearhardt@gmail.com>
2024-08-07 01:20:37 -0600

    BrowserNavBlock

--- src/atoms/_exports.ts
+++ src/atoms/_exports.ts
@@ -4,6 +4,8 @@ export {
   type ActionButtonProps,
   type ActionProps,
   ActionStyleTypes,
+  BrowserNavBlock,
+  type BrowserNavBlockProps,
 } from "../src.deps.ts";
 
 export { Logo, type LogoProps } from "./Logo.tsx";
