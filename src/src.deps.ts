ceaa6ca9ec79d6b0ca57c6f669055a0874919203
mcgear <michael.gearhardt@gmail.com>
2024-11-15 09:47:50 -0700

    ref updates for

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -3,16 +3,16 @@
 // export * from "../../../fathym-deno/atomic-chat/mod.ts";
 // export * from "../../../fathym-deno/atomic-social/mod.ts";
 
-export { delay } from "jsr:@std/async@1/delay";
+export { delay } from "jsr:@std/async@1.0.8/delay";
 
 export { type ComponentChildren, type JSX } from "preact";
 export * from "preact/hooks";
 // export { Signal, useComputed, useSignal } from "npm:@preact/signals@1.2.3";
 // export {} from "npm:@preact/signals-core@1.6.0";
 
-export * from "jsr:@fathym/common@0.2.160";
+export * from "jsr:@fathym/common@0.2.167";
 
-export * from "jsr:@fathym/atomic@0.0.175";
+export * from "jsr:@fathym/atomic@0.0.176";
 // export * from "fathym_atomic_chat/mod.ts";
 // export * from "fathym_atomic_iot/mod.ts";
 // export * from "https://deno.land/x/fathym_atomic_reports@v0.0.29/mod.ts";
