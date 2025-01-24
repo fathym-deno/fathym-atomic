d54692294cddb48ee8e1476c909b0d8bd0e9c323
mcgear <michael.gearhardt@gmail.com>
2025-01-24 11:34:33 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -12,7 +12,7 @@ export * from "preact/hooks";
 
 export * from "jsr:@fathym/common@0.2.168";
 
-export * from "jsr:@fathym/atomic@0.0.177";
+export * from "jsr:@fathym/atomic@0.0.178";
 // export * from "fathym_atomic_chat/mod.ts";
 // export * from "fathym_atomic_iot/mod.ts";
 // export * from "https://deno.land/x/fathym_atomic_reports@v0.0.29/mod.ts";
